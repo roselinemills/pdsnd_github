d4d3d55caa7b0ea239b274774fa7d88851f05988
roseline.mills <roseline.mills@gmail.com>
2021-03-22 18:52:33 +0000

    Added comment on extracting hour

--- bikeshare.py
+++ bikeshare.py
@@ -70,6 +70,7 @@ def load_data(city, month, day):
 # convert the Start Time column to datetime
     df['Start Time'] = pd.to_datetime(df['Start Time'])
 # extract month and day of week from Start Time to create new columns
+#extract hour from Start Time to create new column
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.weekday_name
     df['hour'] = df['Start Time'].dt.hour
